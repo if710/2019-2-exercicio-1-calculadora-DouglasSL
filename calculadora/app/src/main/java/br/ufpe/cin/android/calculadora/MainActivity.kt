408db75f82d8ebfce0e04a42a387dbd75f7a7137
Douglas Soares <dsl@cin.ufpe.br>
2019-08-18 17:55:01 -0300

    exercise 1

--- calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
@@ -2,14 +2,69 @@ package br.ufpe.cin.android.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set buttons listeners
+        btn_0.setOnClickListener { text_calc.append(btn_0.text) }
+        btn_1.setOnClickListener { text_calc.append(btn_1.text) }
+        btn_2.setOnClickListener { text_calc.append(btn_2.text) }
+        btn_3.setOnClickListener { text_calc.append(btn_3.text) }
+        btn_4.setOnClickListener { text_calc.append(btn_4.text) }
+        btn_5.setOnClickListener { text_calc.append(btn_5.text) }
+        btn_6.setOnClickListener { text_calc.append(btn_6.text) }
+        btn_7.setOnClickListener { text_calc.append(btn_7.text) }
+        btn_8.setOnClickListener { text_calc.append(btn_8.text) }
+        btn_9.setOnClickListener { text_calc.append(btn_9.text) }
+
+        btn_Dot.setOnClickListener { text_calc.append(btn_Dot.text) }
+        btn_RParen.setOnClickListener { text_calc.append(btn_RParen.text) }
+        btn_LParen.setOnClickListener { text_calc.append(btn_LParen.text) }
+        btn_Power.setOnClickListener { text_calc.append(btn_Power.text) }
+        btn_Divide.setOnClickListener { text_calc.append(btn_Divide.text) }
+        btn_Multiply.setOnClickListener { text_calc.append(btn_Multiply.text) }
+        btn_Add.setOnClickListener { text_calc.append(btn_Add.text) }
+        btn_Subtract.setOnClickListener { text_calc.append(btn_Subtract.text) }
+
+        btn_Clear.setOnClickListener { text_calc.text.clear() }
+
+        btn_Equal.setOnClickListener {
+            // treat exception case the expression is invalid
+            try {
+                var expResult = eval(text_calc.text.toString())
+                text_info.setText(expResult.toString())
+            } catch (exception : Throwable){
+                var toast = Toast.makeText(getApplicationContext(), "Invalid expression!",Toast.LENGTH_LONG)
+                toast.show()
+            }
+        }
     }
 
+    // overrides saveInstanceState method to save specific informations
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        val textInfo = text_info.text
+        outState?.putCharSequence("savedTextInfo", textInfo)
+        val textCalc = text_calc.text
+        outState?.putCharSequence("savedTextCalc", textCalc)
+    }
+
+    // overrides restoreInstaceState to get the instances that were saved in the saveInstanceState method
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val textInfo = savedInstanceState?.getCharSequence("savedTextInfo")
+        text_info.setText(textInfo)
+        val textCalc = savedInstanceState?.getCharSequence("savedTextCalc")
+        text_calc.setText(textCalc)
+    }
 
     //Como usar a função:
     // eval("2+2") == 4.0
